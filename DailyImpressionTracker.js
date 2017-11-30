0242f33f9cdad1b9cb08b6ecc31f4824e0b71847
Eric King <cg100146@comporium.com>
2017-11-30 16:27:23 -0500

    Psuedo-coded

---
+++ DailyImpressionTracker.js
@@ -0,0 +1,11 @@
+// If # of daily impressions fall below 50% of daily goal == Priority #2
+
+// Utilize 50+ Workaround
+// Select Current Account - Display
+// Calculate Daily Impression Goal
+    // How to do this?
+    // Has to tie in with a spreadsheet to get values for calculation
+    // Use budget script calculations as a guide
+// Condition to check if the value is below 50% of the daily goal
+    // If it is log the account/campaign/impressions
+    // Notify the provided employee to take a look at the account
